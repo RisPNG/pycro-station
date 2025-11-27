ef813d8a818d5d3b932310064973e423ee81f34c
RisPNG <ris@poppybit.com>
2025-11-27 16:25:35 +0800

    update script

--- pycrostation.sh
+++ pycrostation.sh
@@ -1,5 +1,14 @@
-src/Pynano-3_11_14/bin/python3.11 -m venv "src/venv"
+# Create venv only if it doesn't exist
+if [ ! -d "src/venv" ]; then
+    src/Pynano-3_11_14/bin/python3.11 -m venv "src/venv"
+fi
+
+# Activate the venv
 . src/venv/bin/activate
+
+# Upgrade pip and install requirements
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# Run the script
 python src/main.py
