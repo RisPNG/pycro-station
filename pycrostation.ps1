c2e4bf90ca6a2f6a010d7825f7cb15323b4872d2
Ris <ris@poppybit.com>
2025-11-28 09:31:38 +0800

    Windows support!

---
+++ pycrostation.ps1
@@ -0,0 +1,14 @@
+# Create venv only if it doesn't exist
+if (-not (Test-Path "src\venv-win")) {
+    src\MsPy-3_11_14-win\python.exe -m venv "src\venv-win"
+}
+
+# Activate the venv
+& src\venv-win\Scripts\Activate.ps1
+
+# Upgrade pip and install requirements
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run the script
+python src\main.py
